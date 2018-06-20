358bf6947e67760ffbbf00dbf66a30926f10b188
BogdanBessit <paradox1859@gmail.com>
2018-06-20 19:29:02 +0700

    The first commit

---
+++ examples/image.ino
@@ -0,0 +1,35 @@
+#include <ADNS2610.h> 
+
+/*Arduino pins*/
+#define     SCK_PIN             2
+#define     SDIO_PIN            3
+
+using namespace ADNS2610;
+
+
+Sensor opticalSensor( SCK_PIN, SDIO_PIN );//create sensor object
+
+void setup()
+{
+    Serial.begin( 9600 );
+}
+
+
+void loop()
+{
+    uint8_t* frame = new uint8_t;
+
+    /*returns number of pixels have been read;
+     *could be less than 324*/
+    uint16_t nP = optical.GetImage( frame );
+    /*Send data to serial port*/
+    for( uint16_t i = 0; i < nP; i++ )
+    {
+        Serial.print( frame[ i ] );
+        Serial.print( " " );
+    }
+    Serial.println();
+    delay( 100 );
+
+    delete frame;
+}
