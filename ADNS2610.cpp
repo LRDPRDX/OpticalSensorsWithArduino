a887b6ce3bd8be4c2eae06eb63e5db8a165495d3
LRDPRDX <paradox1859@gmail.com>
2021-03-10 04:22:26 +0700

    Remove trailing spaces

--- ADNS2610.cpp
+++ ADNS2610.cpp
@@ -6,7 +6,6 @@ namespace ADNS2610
     void Sensor::SetAddress( uint8_t address, DataDirection direction ) const
     {
         pinMode( sdioPin, OUTPUT );
-    
         /*Specify data direction:
          *MSB = 0   ===>   read operation
          *MSB = 1   ===>   write operation*/
@@ -19,7 +18,7 @@ namespace ADNS2610
                 address &= ~0b10000000 ;
                 break;
         }
-    
+
         for( uint8_t mask = 0b10000000; mask; mask >>= 1 )
         {
             digitalWrite( sckPin, HIGH );
@@ -30,19 +29,18 @@ namespace ADNS2610
         /*It's not necessary to wait for 100 us
          *because digitalWrite takes a lot of time itself*/
          //delayMicroseconds( 100 );
-    
+         //
         /*Set SDIO pin in High-Z state
          *( see p.13 in the datasheet )*/
          pinMode( sdioPin, INPUT );
     }
-    
-    
+
     uint8_t Sensor::ReadRegister( uint8_t address ) const
     {
         SetAddress( address, FROM_SENSOR );
-    
+
         uint8_t data = 0b00000000;
-    
+
         /*Read data from a register*/
         pinMode( sdioPin, INPUT );
         for( uint8_t mask = 0b10000000; mask; mask >>= 1 )
@@ -50,25 +48,25 @@ namespace ADNS2610
             digitalWrite( sckPin, HIGH );
             digitalWrite( sckPin, LOW );
             digitalWrite( sckPin, HIGH );
-    
+
             if( digitalRead( sdioPin ) )
             {
                 data |= mask;
             }
         }
-    
+
         /*Wait for 0.25 microseconds between read and either
          *read or write operations (see p.15 of the datasheet)*/
         delayMicroseconds( 1 );
-    
+
         return data;
     }
-    
-    
+
+
     void Sensor::WriteRegister( uint8_t address, uint8_t data ) const
     {
         SetAddress( address, TO_SENSOR );
-    
+
         pinMode( sdioPin, OUTPUT );
         /*Write data to a register*/
         for( uint8_t mask = 0b10000000; mask; mask >>= 1 )
@@ -82,12 +80,12 @@ namespace ADNS2610
          *because digitalWrite takes a lot of time itself*/
          //delayMicroseconds( 100 );
     }
-    
-    
+
+
     void Sensor::SetConfigBit( uint8_t bit, bool state ) const
     {
         uint8_t currentState = ReadRegister( CFG_REG_ADDR );
-    
+
         if( state )
         {
             currentState |= ( 1 << bit );
@@ -96,10 +94,10 @@ namespace ADNS2610
         {
             currentState &= ~( 1 << bit );
         }
-    
+
         WriteRegister( CFG_REG_ADDR, currentState );
     }
-    
+
 
     /*Public methods*/
     bool Sensor::IsAwake() const
@@ -107,77 +105,66 @@ namespace ADNS2610
         //Returns the status of the LED
         return ReadRegister( STS_REG_ADDR ) & 0b00000001;
     }
-    
-    
+
+
     signed char Sensor::GetDX() const
     {
         //Returns x-shift
         return (signed char)ReadRegister( DX_REG_ADDR );
     }
-    
-    
+
     signed char Sensor::GetDY() const
     {
         //Returns y-shift
         return (signed char)ReadRegister( DY_REG_ADDR );
     }
-    
-    
+
     uint8_t Sensor::GetSQUAL() const
     {
         //Returns number of features in the frame
         //SetAwakeLED();
-    
         return ReadRegister( SQL_REG_ADDR ) * 2;
-    
         //SetNormalLED();
     }
-    
+
     uint8_t Sensor::GetMaxPixel() const
     {
         //Returns maximum pixel value in the frame
         //SetAwakeLED();
-    
         return ReadRegister( MXP_REG_ADDR ) & 0b00111111;
-    
         //SetNormalLED();
     }
-    
-    
+
     uint8_t Sensor::GetMinPixel() const
     {
         //Returns minimum pixel value in the frame
         //SetAwakeLED();
-    
         return ReadRegister( MNP_REG_ADDR ) & 0b00111111;
-    
         //SetNormalLED();
     }
-    
-    
+
     uint8_t Sensor::GetPixelAverage() const
     {
         //Returns average of the pixel values
     //    SetAwakeLED();
-    
         return ReadRegister( PXLSUM_REG_ADDR ) * 0.395;
     }
-    
-    
+
+
     uint16_t Sensor::GetImage( uint8_t* frame ) const
     {
-    
+
         //Returns number of pixels have been read
         SetAwakeLED();
-    
+
         /*Write anything to pixel data register
          *( see p.23 in the datasheet)*/
         WriteRegister( PXLDAT_REG_ADDR, 0x00 );
-    
+
         uint16_t nPixelsRead = 0;//Monitor how many pixels were read
         while( true )
         {
-            uint8_t pixelData = ReadRegister( PXLDAT_REG_ADDR );    
+            uint8_t pixelData = ReadRegister( PXLDAT_REG_ADDR );
             /*Continue reading only if there is valid data
              *(see p.23 in the datasheet)*/
             if( pixelData & ( 1 << PXLDAT_REG_VLD ) )
@@ -202,15 +189,13 @@ namespace ADNS2610
             }
         }
     }
-    
-    
+
     void Sensor::SetAwakeLED() const
     {
         //Sets LED in "always awake" mode
         SetConfigBit( CFG_REG_LED, 1 );
     }
-    
-    
+
     void Sensor::SetNormalLED() const
     {
         //Sets LED in normal mode
--- ADNS2610.h
+++ ADNS2610.h
@@ -20,16 +20,16 @@ namespace ADNS2610
     const uint8_t     STRUPR_REG_ADDR   = 0x09;//Shutter upper register
     const uint8_t     STRLWR_REG_ADDR   = 0x0A;//Shutter lower register
     const uint8_t     INVPRD_REG_ADDR   = 0x0B;//Inverse product code register
-    
+
     //Bits of the configuration register
     const uint8_t     CFG_REG_LED       = 0x00;//LED control bit
     const uint8_t     CFG_REG_PWRDWN    = 0x06;//Power down bit
     const uint8_t     CFG_REG_RST       = 0x07;//Reset config bit
-    
+
     //Bits of the Pixel data register
     const uint8_t     PXLDAT_REG_VLD    = 0x06;//Valid data bit
     const uint8_t     PXLDAT_REG_SOF    = 0x07;//Start of frame bit
-    
+
     //Auxiliaries
     const uint16_t    N_PIXELS          = 324;//Sensor's resolution in pixels
     const uint8_t     N_ROWS            = 18;
@@ -37,7 +37,7 @@ namespace ADNS2610
 
 
     /*A sensor is represented by this class*/
-    class Sensor 
+    class Sensor
     {
         protected :
             uint8_t         sckPin;//clock pin
@@ -48,7 +48,7 @@ namespace ADNS2610
             uint8_t         ReadRegister( uint8_t address ) const;
             void            WriteRegister( uint8_t address, uint8_t data ) const;
             void            SetConfigBit( uint8_t bit, bool state ) const;
-    
+
         public :
             Sensor( uint8_t sckPin, uint8_t sdioPin ) :
                 sckPin( sckPin ),
@@ -57,7 +57,7 @@ namespace ADNS2610
                 pinMode( sckPin, OUTPUT );
                 pinMode( sdioPin, INPUT );
             }
-    
+
             bool            IsAwake( ) const;
             signed char     GetDX( ) const;
             signed char     GetDY( ) const;
@@ -66,7 +66,7 @@ namespace ADNS2610
             uint8_t         GetMinPixel( ) const;
             uint8_t         GetPixelAverage( ) const;
             uint16_t        GetImage( uint8_t* frame ) const;
-    
+
             void            SetAwakeLED() const;
             void            SetNormalLED() const;
     };//Sensor
